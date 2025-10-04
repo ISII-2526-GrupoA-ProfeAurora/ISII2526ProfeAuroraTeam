fac7f40a9fca4ebf7f9b43da726030150b1cbf02
AURORA MACIAS OJEDA <Aurora.Macias@alu.uclm.es>
2025-10-04 17:52:35 +0200

    Constructores Genre

--- src/AppForSEII2526.API/Models/Genre.cs
+++ src/AppForSEII2526.API/Models/Genre.cs
@@ -2,11 +2,20 @@
 {
     public class Genre
     {
+        public Genre()
+        {
+        } // Constructor por defecto requerido por EF Core
+
+        public Genre(string name)
+        {
+            Name = name;
+        }
+
         public int Id { get; set; }
         [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters.", MinimumLength = 4)]
         public string Name { get; set; }
 
-        public List<Movie> Movies { get; set; }
+        public List<Movie> Movies { get; set; } = new(); // Inicialización segura
     }
 
 }
