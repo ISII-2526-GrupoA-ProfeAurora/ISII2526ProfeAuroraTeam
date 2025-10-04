32764406d0bf00dd5ac5ba17055e6e25492f1118
AURORA MACIAS OJEDA <Aurora.Macias@alu.uclm.es>
2025-10-04 18:10:36 +0200

    Añadidos constructores clase RentalItem

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -4,6 +4,33 @@
 
     public class RentalItem
     {
+        public RentalItem()
+        {
+        }
+
+        public RentalItem(Movie movie, Rental rent)
+        {
+            Movie = movie;
+            MovieId = movie.Id;
+            Rent = rent;
+            RentalId = rent.Id;
+        }
+
+        public RentalItem(Movie movie, Rental rent, string? description) : this(movie, rent)
+        {
+            Description = description;
+            PriceForRenting = movie.PriceForRenting;
+        }
+
+        public RentalItem(int movieId, Rental rental, double priceForRenting)
+        {
+            MovieId = movieId;
+            Rent = rental;
+            PriceForRenting = priceForRenting;
+        }
+
+        public RentalItem(int movieId, Rental rental, double priceForRenting, string? description) : this(movieId, rental, priceForRenting) => Description = description;
+
         public int MovieId { get; set; }
         public Movie Movie { get; set; }
 
