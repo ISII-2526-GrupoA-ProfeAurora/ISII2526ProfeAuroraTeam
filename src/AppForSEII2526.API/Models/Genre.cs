3af274ee95a377529d4a132919f65bc01d43df71
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-10-04 11:13:27 +0200

    6 crear clase genre (#9)
    * #6 Primera versión de clase Genre
    
    * Añadida clase Genre
    
    2o commit

---
+++ src/AppForSEII2526.API/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters.", MinimumLength = 4)]
+        public string Name { get; set; }
+    }
+
+}
