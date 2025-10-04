d814184ecb5b7e0499fc9db277ea1183640d6223
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-10-04 18:14:45 +0200

    17 añadir constructores y revisar relaciones y atributos (#18)
    * Añadida relación 1-M entre Movie y Genre, y cambio atrasado en ApllicationDBContext
    
    * Añadir alternate key en Genre
    
    * Constructores Genre
    
    * Nombre único en Genre
    
    * Constructores de clase Movie
    
    * Titulo como campo unique en Movie
    
    * Añadidos constructores clase Rental
    
    * Añadidos constructores clase RentalItem

--- src/AppForSEII2526.API/Models/Genre.cs
+++ src/AppForSEII2526.API/Models/Genre.cs
@@ -1,12 +1,23 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    //Title is unique for each instance of Movie
+    [Index(nameof(Name), IsUnique = true)]
     public class Genre
     {
+        public Genre()
+        {
+        } // Constructor por defecto requerido por EF Core
+
+        public Genre(string name)
+        {
+            Name = name;
+        }
+
         public int Id { get; set; }
         [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters.", MinimumLength = 4)]
         public string Name { get; set; }
 
-        public List<Movie> Movies { get; set; }
+        public List<Movie> Movies { get; set; } = new(); // Inicialización segura
     }
 
 }
--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -2,6 +2,23 @@
 {
     public class Rental
     {
+        public Rental()
+        {
+        }
+
+        public Rental(string customerUserName, string customerNameSurname, string deliveryAddress, DateTime rentalDate, DateTime rentalDateFrom, DateTime rentalDateTo, PaymentMethodTypes paymentMethod, ApplicationUser applicationUser)
+        {
+            //autogenerado
+            CustomerUserName = customerUserName;
+            CustomerNameSurname = customerNameSurname;
+            DeliveryAddress = deliveryAddress;
+            RentalDate = rentalDate;
+            RentalDateFrom = rentalDateFrom;
+            RentalDateTo = rentalDateTo;
+            PaymentMethod = paymentMethod;
+            ApplicationUser = applicationUser;
+        }
+
         public int Id { get; set; }
 
         public string CustomerUserName { get; set; }
