e5eab522013c2ea845008155e51dfae73078bfc4
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-10-04 19:01:54 +0200

    17 añadir constructores y revisar relaciones y atributos (2) (#19)
    * Añadida relación 1-M entre Movie y Genre, y cambio atrasado en ApllicationDBContext
    
    * Añadir alternate key en Genre
    
    * Constructores Genre
    
    * Nombre único en Genre
    
    * Constructores de clase Movie
    
    * Titulo como campo unique en Movie
    
    * Añadidos constructores clase Rental
    
    * Añadidos constructores clase RentalItem
    
    * Ajuste campos ApplicationUser (errores migration)

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,8 +4,23 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
-    public string Name { get; set; }
 
-    public string Surname { get; set; }
+    public ApplicationUser()
+    {
+    }
+    public ApplicationUser(string id, string name, string surname, string userName)
+    {
+        Id = id;
+        Name = name;
+        Surname = surname;
+        UserName = userName;
+        Email = userName;
+    }
+
+    [Display(Name = "Name")]
+    public string? Name { get; set; }
+
+    [Display(Name = "Surname")]
+    public string? Surname { get; set; }
 
 }
