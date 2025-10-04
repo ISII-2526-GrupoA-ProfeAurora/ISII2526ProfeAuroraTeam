0be0e21f17e7ec0a3a5c59f84d06ef4be3353f55
AURORA MACIAS OJEDA <Aurora.Macias@alu.uclm.es>
2025-10-04 11:41:40 +0200

    Añadida clase Movie (atributos)

---
+++ src/AppForSEII2526.API/Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(100, ErrorMessage = "Title name cannot be longer than 100 characters.")]
+        public string Title { get; set; }
+        
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, float.MaxValue, ErrorMessage = "Minimum price is 0.5 ")]
+        [Display(Name = "Price For Purchase")]
+        [Precision(10, 2)]
+        public decimal PriceForPurchase { get; set; }
+        
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for Purchase is 1")]
+        [Display(Name = "Quantity For Purchase")]
+        public int QuantityForPurchase { get; set; }
+        
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, 100, ErrorMessage = "Minimum is 0.5 and maximum 100")]
+        [Display(Name = "Price For Renting")]
+        public decimal PriceForRenting { get; set; }
+        
+        [Display(Name = "Quantity For Renting")]
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for Renting is 1")]
+        public int QuantityForRenting { get; set; }
+        
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        
+        public Genre Genre { get; set; }
+
+
+
+    }
+}
