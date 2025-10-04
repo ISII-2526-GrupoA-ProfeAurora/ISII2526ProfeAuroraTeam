4fb868108a6073e0387ca67e36d3a32c5122789b
Aurora Macías Ojeda <auroramo@users.noreply.github.com>
2025-10-04 13:48:23 +0200

    Creadas clases Rental y RentalItems (pendientes algunas configuraciones) (#12)

---
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(MovieId), nameof(RentalId))]
+
+    public class RentalItem
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int RentalId { get; set; }
+        public Rental Rent { get; set; }
+
+        [StringLength(100, ErrorMessage = "Title name cannot be longer than 100 characters.")]
+        public string? Description { get; set; }
+
+        public double PriceForRenting { get; set; }
+
+    }
+}
